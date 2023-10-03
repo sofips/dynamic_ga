a358f65ceb6253bc83e3d5ceebd15c5b6aba4993
sofips <sofiperon98@gmail.com>
2023-10-03 13:09:42 -0300

    add paper actions

--- dgamod.py
+++ dgamod.py
@@ -353,6 +353,113 @@ def time_evolution(solution,propagadores,nh,graph=False,filename=False):
     return fid_evolution_array             
         
             
-           
+################
+#acciones zhang#
+################
+
+def diagonales_paper(bmax, i, nh):
+    b = np.full(nh, 0)
+
+    if (i == 1):
+        b[0] = 1
+
+    elif (i == 2):
+
+        b[1] = 1
+
+    elif (i == 3):
+
+        b[0] = 1
+        b[1] = 1
+
+    elif (i == 4):
+
+        b[2] = 1  # correccion
+
+    elif (i == 5):
+
+        b[0] = 1
+        b[2] = 1
+
+    elif (i == 6):
+
+        b[1] = 1
+        b[2] = 1
+
+    elif (i == 7):
+
+        b[0] = 1
+        b[1] = 1
+        b[2] = 1
+
+    elif (i == 8):
+
+        b[nh-3] = 1
+
+    elif (i == 9):
+
+        b = np.full(nh, -1)
+        b[nh-2] = 1
+
+    elif (i == 10):
+
+        b[nh-3] = 1
+        b[nh-2] = 1
+
+    elif (i == 11):
+
+        b[nh-1] = 1
+
+    elif (i == 12):
+
+        b[nh-3] = 1
+        b[nh-1] = 1
+
+    elif (i == 13):
+
+        b[nh-2] = 1
+        b[nh-1] = 1
+
+    elif (i == 14):
+
+        b[nh-3] = 1
+        b[nh-2] = 1
+        b[nh-1] = 1
+
+    elif (i == 15):
+
+        b[0] = 1
+        b[1] = 1
+        b[2] = 1
+        b[nh-3] = 1
+        b[nh-2] = 1
+        b[nh-1] = 1
+
+    else:
+        b = np.full(nh, 0.)  # correccion
+
+    b = bmax*b
+
+    return b
+
+def actions_paper(bmax, nh):
+
+    mat_acc = np.zeros((16, nh, nh))
+
+    for i in range(0, 16):
+
+        b = diagonales_paper(bmax, i, nh)
+
+        J =  1 #[-0.5*np.sqrt((nh-k)*k) for k in np.arange(1,nh,1)]
+
+        for k in range(0, nh-1):
+            mat_acc[i, k, k+1] = J
+            mat_acc[i, k+1, k] = mat_acc[i, k, k+1]
+
+        for k in range(0, nh):
+
+            mat_acc[i, k, k] = b[k]
+
+    return mat_acc
          
 
