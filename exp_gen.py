c56046e9a0751ce6f1dd912bb9b09eb8551280e5
sofips <sofiperon98@gmail.com>
2024-04-08 15:18:14 -0300

    script for separate instancing

--- exp_gen.py
+++ exp_gen.py
@@ -17,18 +17,34 @@ n =  int(sys.argv[1])     # number of spins in the system
 dt = 0.15  # length of temporal steps
 b = 100    # magnetic field strength
 
-config['system_parameters'] = {'n': str(n),
-                               'dt': str(dt),
-                               'b': str(b) }
-
-
 # genetic algorithm parameters
 
 num_generations = 2000
 num_genes = 5*n  # chosen to allow a transmission time of 2n
 sol_per_pop = 2000          
 fidelity_tolerance = 0.95
-saturation = 30
+saturation = 5
+
+# crossover and parent selection
+num_parents_mating = sol_per_pop//10
+parent_selection_type = 'sss'
+keep_elitism = sol_per_pop//5
+crossover_type = 'uniform'
+crossover_probability = 0.9
+
+# other mutation parameters
+
+mutation_probability = 0.99
+mutation_num_genes = n
+
+# execution and results saving
+directory = sys.argv[2]
+n_samples = 1
+
+
+config['system_parameters'] = {'n': str(n),
+                               'dt': str(dt),
+                               'b': str(b) }
 
 config['ga_initialization'] = {'num_generations': str(num_generations),
                                'num_genes': str(num_genes),
@@ -36,12 +52,6 @@ config['ga_initialization'] = {'num_generations': str(num_generations),
                                'fidelity_tolerance': str(fidelity_tolerance),
                                'saturation': str(saturation)}
 
-# crossover and parent selection
-num_parents_mating = sol_per_pop//5
-parent_selection_type = 'sss'
-keep_elitism = sol_per_pop//10
-crossover_type = 'uniform'
-crossover_probability = 0.6
 
 config['parent_selection'] = {'num_parents_mating': str(num_parents_mating),
                               'parent_selection_type': parent_selection_type,
@@ -51,22 +61,13 @@ config['crossover'] = {'crossover_type': crossover_type,
                        'crossover_probability': str(crossover_probability)}
 
 
-# other mutation parameters
-
-mutation_probability = 0.99
-mutation_num_genes = 2
-
 config['mutation'] = {'mutation_probability': str(mutation_probability),
                       'mutation_num_genes': str(mutation_num_genes)}
 
 
-# execution and results saving
-directory = sys.argv[2]
-n_samples = 5
-
 config['saving'] = {'directory': directory,
                     'n_samples': str(n_samples)}
-script = 'dc_ga.py'
+script = 'dc_ga_separate_instances.py'
 
 isExist = os.path.exists(directory)
 if not isExist:
