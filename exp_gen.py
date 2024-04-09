60ea3ac1fa1c4afe3fd0ebb6f93e8ea9be404b8f
sofips <sofiperon98@gmail.com>
2024-04-09 12:20:31 -0300

    formatting and modified parameters to use separate instances script

--- exp_gen.py
+++ exp_gen.py
@@ -1,8 +1,8 @@
-'''
+"""
 Generates configuration files for different runs of 
 genetic algorithm implementation, using configparser
 library. Creates directory and saves script in it.
-'''
+"""
 
 import configparser
 import os
@@ -13,24 +13,24 @@ config = configparser.ConfigParser()
 
 # system parameters
 
-n =  int(sys.argv[1])     # number of spins in the system
+n = int(sys.argv[1])  # number of spins in the system
 dt = 0.15  # length of temporal steps
-b = 100    # magnetic field strength
+b = 100  # magnetic field strength
 
 # genetic algorithm parameters
 
 num_generations = 2000
-num_genes = 5*n  # chosen to allow a transmission time of 2n
-sol_per_pop = 2000          
-fidelity_tolerance = 0.95
+num_genes = 5 * n  # chosen to allow a transmission time of 2n
+sol_per_pop = 3000
+fidelity_tolerance = 0.05
 saturation = 5
 
 # crossover and parent selection
-num_parents_mating = sol_per_pop//10
-parent_selection_type = 'sss'
-keep_elitism = sol_per_pop//5
-crossover_type = 'uniform'
-crossover_probability = 0.9
+num_parents_mating = sol_per_pop // 10
+parent_selection_type = "sss"
+keep_elitism = sol_per_pop // 10
+crossover_type = "uniform"
+crossover_probability = 0.8
 
 # other mutation parameters
 
@@ -38,62 +38,68 @@ mutation_probability = 0.99
 mutation_num_genes = n
 
 # execution and results saving
+population_histograms = False
 directory = sys.argv[2]
-n_samples = 1
+n_samples = 10
 
 
-config['system_parameters'] = {'n': str(n),
-                               'dt': str(dt),
-                               'b': str(b) }
+config["system_parameters"] = {"n": str(n), "dt": str(dt), "b": str(b)}
 
-config['ga_initialization'] = {'num_generations': str(num_generations),
-                               'num_genes': str(num_genes),
-                               'sol_per_pop': str(sol_per_pop),
-                               'fidelity_tolerance': str(fidelity_tolerance),
-                               'saturation': str(saturation)}
-
-
-config['parent_selection'] = {'num_parents_mating': str(num_parents_mating),
-                              'parent_selection_type': parent_selection_type,
-                              'keep_elitism': str(keep_elitism)}
-
-config['crossover'] = {'crossover_type': crossover_type,
-                       'crossover_probability': str(crossover_probability)}
+config["ga_initialization"] = {
+    "num_generations": str(num_generations),
+    "num_genes": str(num_genes),
+    "sol_per_pop": str(sol_per_pop),
+    "fidelity_tolerance": str(fidelity_tolerance),
+    "saturation": str(saturation),
+}
 
 
-config['mutation'] = {'mutation_probability': str(mutation_probability),
-                      'mutation_num_genes': str(mutation_num_genes)}
+config["parent_selection"] = {
+    "num_parents_mating": str(num_parents_mating),
+    "parent_selection_type": parent_selection_type,
+    "keep_elitism": str(keep_elitism),
+}
+
+config["crossover"] = {
+    "crossover_type": crossover_type,
+    "crossover_probability": str(crossover_probability),
+}
 
 
-config['saving'] = {'directory': directory,
-                    'n_samples': str(n_samples)}
-script = 'dc_ga_separate_instances.py'
+config["mutation"] = {
+    "mutation_probability": str(mutation_probability),
+    "mutation_num_genes": str(mutation_num_genes),
+}
+
+
+config["saving"] = {
+    "population_histograms": str(population_histograms),
+    "directory": directory,
+    "n_samples": str(n_samples),
+}
+script = "dc_ga_separate_instances.py"
 
 isExist = os.path.exists(directory)
 if not isExist:
     os.mkdir(directory)
 else:
-    print('Warning: Directory already existed')
+    print("Warning: Directory already existed")
 
 src = os.path.dirname(os.path.abspath(__file__))
 script_name = os.path.join(src, script)
-mod_name = os.path.join(src, 'dgamod.py')
+mod_name = os.path.join(src, "dgamod.py")
 
 cmd = f'cp "{script_name}" "{directory}"'
 os.system(cmd)
 cmd = f'cp "{mod_name}" "{directory}"'
 os.system(cmd)
-config_name = directory + '/'+ 'ga'+directory+'.ini'
+config_name = directory + "/" + "ga" + directory + ".ini"
 
-with open(config_name, 'w') as configfile:
+with open(config_name, "w") as configfile:
     config.write(configfile)
 
-script_name = directory + '/' +script
-config_name = 'ga' + directory + '.ini'
+script_name = directory + "/" + script
+config_name = "ga" + directory + ".ini"
 
 cmd = f'python3 "{script_name}" "{config_name}"'
 os.system(cmd)
-
-
-
-
