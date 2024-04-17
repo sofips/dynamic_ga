920a6e62f144cc7960e1f145a69e5ba527f07af8
sofips <sofiperon98@gmail.com>
2024-04-17 10:15:32 -0300

    run high tolerance

--- run_for_n.py
+++ run_for_n.py
@@ -4,8 +4,8 @@ import numpy as np
 dimensions = np.arange(6,20,1)
 
 for dim in dimensions:
-    dirname = 'n{}_si'.format(dim)
-    cmd = 'nice -20 python3 exp_gen.py {} {} >out{}_si.out &'.format(dim, dirname, dim)
+    dirname = 'n{}_og_ht'.format(dim)
+    cmd = 'nice -20 python3 exp_gen.py {} {} >out{}_oght.out &'.format(dim, dirname, dim)
     os.system(cmd)
     os.system('bg')
     os.system('disown')
