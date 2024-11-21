69fcc58365efd3ff2b50c0d44102d79ab9b485ba
sofips <sofiperon98@gmail.com>
2024-11-21 15:50:15 -0300

    statistics generation

--- gastats.py
+++ gastats.py
@@ -2,7 +2,7 @@ import numpy as np
 import os
 
 for i in np.arange(0, 10, 1):
-    dirname = 'n13_stats_oaps0_lb_{}'.format(i)
+    dirname = 'n13_stats_oaps0_extra_lb{}'.format(i)
     cmd = 'nice -20 python3 exp_gen.py 13 {} >> out_{} &'.format(dirname,i)
     os.system(cmd)
     os.system('bg')
