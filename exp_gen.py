cf923601599b5726909bf36763d42de19a60201e
sofips <sofiperon98@gmail.com>
2025-02-21 11:59:04 -0300

    add all files

--- exp_gen.py
+++ exp_gen.py
@@ -13,7 +13,7 @@ config = configparser.ConfigParser()
 
 # system parameters
 
-n = 16 #int(sys.argv[1])  # number of spins in the system
+n = 64 #int(sys.argv[1])  # number of spins in the system
 dt = 0.15  # length of temporal steps
 b = 100  # magnetic field strength
 #speed_fraction = 1.1  # fraction of qsl speed
