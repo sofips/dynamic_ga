636428edabd7b01b1741d4a07e4101119ec4eb87
sofips <sofiperon98@gmail.com>
2025-03-04 13:32:09 -0300

    new results

--- run_for_n.py
+++ run_for_n.py
@@ -4,7 +4,7 @@ import numpy as np
 dimensions = [8,16,32,64,128] #np.arange(10,20,1)
 
 for dim in dimensions:
-    dirname = 'n{}_profiling_001'.format(dim)
+    dirname = 'n{}_gpu'.format(dim)
     cmd = 'nice -20 python3 exp_gen.py {} {} >out{}.out &'.format(dim, dirname, dim)
     os.system(cmd)
     os.system('bg')
